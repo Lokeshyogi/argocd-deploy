147638f7e468175f4907246e2892fe5c9aeac06a
Lokesh Yogi <36193461+Lokeshyogi@users.noreply.github.com>
2021-02-08 19:46:45 +0530

    Delete argocd-rollouts directory

--- argocd-rollouts/argoDeployment/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source ./init.sh
-kubectl apply -f ./argoDeployment/namespace.yaml
-kubectl apply -n argocd-ns -f https://raw.githubusercontent.com/argoproj/argo-cd/v1.8.3/manifests/install.yaml
-kubectl apply -f ./argoDeployment/ingress.yml -n argocd-ns
-
